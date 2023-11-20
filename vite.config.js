cd149c6ceab220fa3e1dc87490c01ce9420695e9
Aderauwu04 <aderabg1@gmail.com>
2023-11-20 18:36:50 -0400

    Ajustes en los puertos de vite

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
+    host: true,
+    strictPort: true,
     port: 8080,
   },
   plugins: [vue()],
